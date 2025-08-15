e21a89a5bd4f94d70dee949b4fa9baf459afa673
rlovely <rlovely@medallionis.com>
2025-08-15 16:04:58 -0400

    ove MaybeIsSomeConstraint to its own file

---
+++ UnitTests/MaybeIsSomeConstraint.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework.Constraints;
+using RoadieRich.Maybe;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace UnitTests;
+
+[DebuggerNonUserCode]
+public class MaybeIsSomeConstraint<T> : Constraint
+{
+	private readonly Maybe<T> _value;
+
+	public override string Description => $"Maybe.Some({_value})";
+
+	private IEqualityComparer<T>? _comparer = null;
+
+	public MaybeIsSomeConstraint(T value)
+	{
+		_value = value;
+	}
+
+	public MaybeIsSomeConstraint()
+	{
+		_value = Maybe.None;
+	}
+
+	public override ConstraintResult ApplyTo<TActual>(TActual actual)
+	{
+
+		var comparer = _comparer ?? EqualityComparer<T>.Default;
+
+		if (actual is Maybe<T>.Some some)
+		{
+			if (_value is Maybe<T>.Some someValue)
+			{
+				return new ConstraintResult(this, actual, comparer.Equals(some.Value, someValue.Value));
+			}
+			else
+				return new ConstraintResult(this, actual, true);
+		}
+		else
+		{
+			return new ConstraintResult(this, actual, false);
+		}
+	}
+
+	public Constraint Using(IEqualityComparer<T> comparer)
+	{
+		_comparer = comparer;
+		return this;
+	}
+}
--- UnitTests/MaybeIsSomeWithoutTypeConstraint.cs
+++ UnitTests/MaybeIsSomeWithoutTypeConstraint.cs
@@ -4,50 +4,6 @@ using System.Collections.Generic;
 
 namespace UnitTests;
 
-public class MaybeIsSomeConstraint<T> : Constraint
-{
-	private readonly Maybe<T> _value;
-
-	public override string Description => $"Maybe.Some({_value})";
-
-	private IEqualityComparer<T>? _comparer = null;
-
-	public MaybeIsSomeConstraint(T value)
-	{
-		_value = value;
-	}
-
-	public MaybeIsSomeConstraint()
-	{
-		_value = Maybe.None;
-	}
-
-	public override ConstraintResult ApplyTo<TActual>(TActual actual)
-	{
-
-		var comparer = _comparer ?? EqualityComparer<T>.Default;
-
-		if (actual is Maybe<T>.Some some)
-		{
-			if (_value is Maybe<T>.Some someValue)
-			{
-				return new ConstraintResult(this, actual, comparer.Equals(some.Value, someValue.Value));
-			}
-			else
-				return new ConstraintResult(this, actual, true);
-		}
-		else
-		{
-			return new ConstraintResult(this, actual, false);
-		}
-	}
-
-	public Constraint Using(IEqualityComparer<T> comparer)
-	{
-		_comparer = comparer;
-		return this;
-	}
-}
 
 public class MaybeIsSomeWithoutTypeConstraint : Constraint
 {
@@ -55,7 +11,7 @@ public class MaybeIsSomeWithoutTypeConstraint : Constraint
 	{
 	}
 
-	public override string Description => "Maybe<...>.Some(...)";
+	public override string Description => "Maybe<*>.Some(...)";
 
 	public override ConstraintResult ApplyTo<TActual>(TActual actual)
 	{
